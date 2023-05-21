1b342753049e22122366f4683b37daf80a22d63c
Pedro Costa <pedroc_aragao@outlook.com>
2023-05-21 11:12:01 -0300

    move physics to server side

--- client/script.js
+++ client/script.js
@@ -1,135 +1,58 @@
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Body = Matter.Body,
-    Composite = Matter.Composite,
-    Mouse = Matter.Mouse,
-    MouseConstraint = Matter.MouseConstraint,
-    Events = Matter.Events,
-    Collision = Matter.Collision
+var socket = io('127.0.0.1:3000')
 
-// create an engine
-var engine = Engine.create({
-    gravity: {
-        scale: 0
-    }
+socket.on('side', (data) => {
+    var playerSide = data
+    console.log(data);
 })
 
-// create a renderer
-var renderer = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: false, // Define se os corpos serão exibidos como arame (true) ou preenchidos (false)
-    }
-})
+// Obtém o elemento canvas e o contexto 2D
+var canvas = document.getElementById('background');
 
-var options = {
-    inertia: Infinity, 
-    restitution: 1,
-    frictionAir: 0,
-    friction: 0
+var ctx = canvas.getContext('2d');
+
+// Define as dimensões do canvas
+canvas.width = 800;
+canvas.height = 600;
+
+// Define as dimensões das raquetes
+var racketWidth = 10;
+var racketHeight = 50;
+
+// Função para desenhar as raquetes
+function drawRacket(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
 }
 
-var playerHeight = 50
-var playerWidth = 10
-var playerAPoints = 0
-var playerBPoints = 0
+// Define as coordenadas e o raio da bolinha
+var ballX = canvas.width / 2; // Coordenada X central
+var ballY = canvas.height / 2; // Coordenada Y central
+var ballRadius = 10;
 
-// create two boxes and a ground
-var ball = Bodies.circle(renderer.options.width / 2, renderer.options.height / 2, 10, options)
-options.isStatic = true
-options.render = { fillStyle: 'red' }
-var playerA = Bodies.rectangle(20, 200, playerWidth, playerHeight, options)
-options.render = { fillStyle: 'blue' }
-var playerB = Bodies.rectangle(780, 50, playerWidth, playerHeight, options)
-options.render = { fillStyle: 'white' }
-var ground = Bodies.rectangle(400, 610, 810, 30, options)
-var floor = Bodies.rectangle(400, 0, 810, 10, options)
-var leftWall = Bodies.rectangle(0, 300, 20, 590, options)
-var rightWall = Bodies.rectangle(800, 300, 20, 590, options)
-
-// add mouse control
-var mouseX = 10,
-    mouseY = 10,
-    mouse = Mouse.create(renderer.canvas),
-    mouseConstraint = MouseConstraint.create(engine, {
-        mouse: mouse,
-        constraint: {
-            stiffness: 0.2,
-            render: {
-                visible: false
-            }
-        }
-    })
-
-// Atualiza as coordenadas do mouse quando o mouse se move
-Events.on(mouseConstraint, 'mousemove', function(event) {
-    mouseX = event.mouse.position.x
-    mouseY = event.mouse.position.y
-})
-
-// Atualiza as coordenadas do mouse quando o mouse é arrastado
-Events.on(mouseConstraint, 'mousedown', function(event) {
-    mouseX = event.mouse.position.x
-    mouseY = event.mouse.position.y
-})
-
-// Atualiza as coordenadas do mouse quando o mouse é solto
-Events.on(mouseConstraint, 'mouseup', function(event) {
-    mouseX = event.mouse.position.x
-    mouseY = event.mouse.position.y
-})
-
-// Atualiza a posição da raquete B com base na posição da bola
-Events.on(engine, 'beforeUpdate', function(event) {
-    var playerBPosition = playerB.position
-    var ballPosition = ball.position
-    if (ballPosition.y < playerBPosition.y) {
-        Body.setPosition(playerB, { x: playerBPosition.x, y: playerBPosition.y - 5 })
-    } else if (ballPosition.y > playerBPosition.y) {
-        Body.setPosition(playerB, { x: playerBPosition.x, y: playerBPosition.y + 5 })
-    }
-    Body.setPosition(playerA, { x: playerA.position.x, y: mouseY })
-})
-
-var resetBallPosition = () => {
-    Body.setPosition(ball, {
-        x: renderer.options.width / 2,
-        y: renderer.options.height / 2
-    })
+// Função para desenhar a bolinha
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  ctx.closePath();
 }
 
-Events.on(engine, 'collisionStart', function(event) {
-    var pairs = event.pairs;
+// Função para desenhar o jogo
+function drawGame() {
+    // Limpa o canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < pairs.length; i++) {
-        var pair = pairs[i];
+    // Define a cor de fundo
+    ctx.fillStyle = 'black';
 
-        if (pair.bodyA === ball && pair.bodyB === rightWall) {
-            playerAPoints++;
-            console.log('playerAPoints=' + playerAPoints)
-            resetBallPosition()
-        } else if (pair.bodyA === ball && pair.bodyB === leftWall) {
-            playerBPoints++;
-            console.log('playerBPoints=' + playerBPoints)
-            resetBallPosition()
-        }
-    }
-});
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-Body.applyForce(ball, {x: ball.position.x, y: ball.position.y}, {x: 0.003, y: 0.003})
+    // Desenha as raquetes vermelha e azul
+    drawRacket(10, canvas.height / 2 - racketHeight / 2, racketWidth, racketHeight, 'red');
+    drawRacket(canvas.width - racketWidth - 10, canvas.height / 2 - racketHeight / 2, racketWidth, racketHeight, 'blue');
+    drawBall()
+}
 
-// add all of the bodies to the world
-Composite.add(engine.world, [playerA, playerB, ball, floor, ground, rightWall, leftWall, mouseConstraint])
-
-// run the renderer
-Render.run(renderer)
-
-// create runner
-var runner = Runner.create()
-
-// run the engine
-Runner.run(runner, engine)
+// Chama a função para desenhar o jogo
+drawGame();
--- server/app.js
+++ server/app.js
@@ -1,14 +1,110 @@
 import { Server } from "socket.io";
+import { Player } from "./player.js";
+import Matter from "matter-js";
+
+
+const playerA = new Player('left'),
+      playerB = new Player('right')
 
 const io = new Server(3000, {
     cors: { origin: '*' },
 })
 
-
 io.on('connection', (socket) => {
-    console.log('id=' + socket.id)
-    socket.on('message', (msg) => {
-        console.log('message received: ' + msg);
-    })
+    if (io.engine.clientsCount == 1) {
+        playerA.socket = socket
+        socket.emit('side', 'left')
+    } else if (io.engine.clientsCount == 2) {
+        socket.emit('side', 'right')
+        playerB.socket = socket
+    } else {
+        socket.disconnect()
+    }
+    console.log('number of connections=' + io.engine.clientsCount);
 })
 
+// module aliases
+var Engine = Matter.Engine,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Composite = Matter.Composite,
+    Events = Matter.Events
+
+// create an engine
+var engine = Engine.create({
+    gravity: {
+        scale: 0
+    }
+})
+
+var screenWidth = 800,
+    screenHeight = 600
+
+var options = {
+    inertia: Infinity, 
+    restitution: 1,
+    frictionAir: 0,
+    friction: 0
+}
+
+var playerHeight = 50
+var playerWidth = 10
+
+// create two boxes and a ground
+var ball = Bodies.circle(screenWidth / 2, screenHeight / 2, 10, options)
+options.isStatic = true
+var racketA = Bodies.rectangle(20, 200, playerWidth, playerHeight, options)
+var racketB = Bodies.rectangle(780, 50, playerWidth, playerHeight, options)
+var ground = Bodies.rectangle(400, 610, 810, 30, options)
+var floor = Bodies.rectangle(400, 0, 810, 10, options)
+var leftWall = Bodies.rectangle(0, 300, 20, 590, options)
+var rightWall = Bodies.rectangle(800, 300, 20, 590, options)
+
+// Atualiza a posição da raquete B com base na posição da bola
+Events.on(engine, 'beforeUpdate', function(event) {
+    var racketBPosition = racketB.position
+    var ballPosition = ball.position
+    if (ballPosition.y < racketBPosition.y) {
+        Body.setPosition(racketB, { x: racketBPosition.x, y: racketBPosition.y - 20 })
+    } else if (ballPosition.y > racketBPosition.y) {
+        Body.setPosition(racketB, { x: racketBPosition.x, y: racketBPosition.y + 20 })
+    }
+})
+
+var resetBallPosition = () => {
+    Body.setPosition(ball, {
+        x: screenWidth / 2,
+        y: screenHeight / 2
+    })
+}
+
+Events.on(engine, 'collisionStart', function(event) {
+    var pairs = event.pairs;
+
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i];
+
+        if (pair.bodyA === ball && pair.bodyB === rightWall) {
+            playerA.makeScore();
+            console.log('playerAPoints=' + playerA.getScore())
+            resetBallPosition()
+        } else if (pair.bodyA === ball && pair.bodyB === leftWall) {
+            playerB.makeScore();
+            console.log('playerBPoints=' + playerB.getScore())
+            resetBallPosition()
+        }
+    }
+});
+
+Body.applyForce(ball, {x: ball.position.x, y: ball.position.y}, {x: 0.003, y: 0.003})
+
+
+// add all of the bodies to the world
+Composite.add(engine.world, [playerA, playerB, ball, floor, ground, rightWall, leftWall])
+
+// create runner
+var runner = Runner.create()
+
+// run the engine
+Runner.run(runner, engine)
